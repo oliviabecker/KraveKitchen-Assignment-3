21019d4b8da3314fd88b7ef4ca726875147a7ea5
Olivia Becker <78497447+oliviabecker@users.noreply.github.com>
2025-10-24 13:59:25 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'krave-kitchen-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/recipes.html',
+  '/recipe.html',
+  '/add.html',
+  '/about.html',
+  '/style.css',
+  '/images/icon-192.png',
+  '/images/icon-512.png',
+  '/images/spaghetti.jpg',
+  '/images/pancakes.jpg',
+  '/images/cheesecake.jpg',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      )
+    )
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
